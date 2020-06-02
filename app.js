53aaf509d602de0daa2125167d6969f070cb2353
Nithin K Joy <nithinjoy121212@gmail.com>
2020-06-02 16:26:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express=require("express")
+const app=express()
+const expressLayout=require("express-ejs-layouts")
+const multer=require("multer")
+const createFolder=require("./output/makedir")
+const cookieParser=require("cookie-parser")
+app.use(cookieParser("its_secret"))
+
+const cookieConfig={
+    signed:true,
+    httpOnly:true
+}
+
+app.use(expressLayout)
+app.set("view engine","ejs")
+
+app.get("/",(req,res)=>{
+    const folderName=Date.now().toString()
+    module.exports=folderName
+    createFolder()
+    res.cookie("nithin_pdf",folderName,cookieConfig)
+    let message=""
+    res.render("upload",{message})
+})
+
+app.post("/download",(req,res)=>{
+    const location=req.signedCookies.nithin_pdf
+    res.download(__dirname+`/output/${location}/Your_pictures.pdf`)
+})
+
+app.post("/upload",(req,res)=>{
+    const location=req.signedCookies.nithin_pdf
+    console.log("location",location)
+    const storage=multer.diskStorage({
+        destination:`./image/${location}`,
+        filename:function(req,file,cb){   
+            cb(null,Date.now()+file.originalname)
+        } 
+    })
+    
+    const upload=multer({
+        storage:storage
+    }).array('myImage', 100)
+    upload(req,res,(err)=>{
+        if(err){
+            res.render("upload",{message:err})
+        }else{
+            console.log(req.file)
+            const ImagesToPDF = require('images-pdf');
+            new ImagesToPDF.ImagesToPDF().convertFolderToPDF(`image/${location}`, `output/${location}/Your_pictures.pdf`);
+            res.render("done")
+        }
+    })
+})
+
+const port=process.env.PORT||3000
+app.listen(port,()=>console.log(`Listening to port ${port}`))
+
---
+++ output/makedir.js
@@ -0,0 +1,13 @@
+const fs=require("fs")
+const path=require("path")
+
+function createFolder(){
+    const folderName=require("../app")
+    console.log("foldername",folderName)
+    fs.mkdir(path.join(__dirname,folderName.toString()),(err)=>{
+        if (err) console.log(err)
+        console.log("Folder created")
+    })
+}
+
+module.exports=createFolder
