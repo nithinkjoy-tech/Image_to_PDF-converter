52ba9b9573736bffdaf82079c21144e290e1ea7c
Nithin K Joy <62066971+nithinkjoy-tech@users.noreply.github.com>
2020-09-07 20:24:13 +0530

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,7 +1,8 @@
+from PIL import Image
 import PIL
 import os
 import sys
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 location = sys.argv[1]
 file_name = os.listdir(f"image/{location}")
@@ -13,5 +14,15 @@ for image in file_name:
     wpercent = (mywidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     img = img.resize((mywidth, hsize), PIL.Image.ANTIALIAS)
+    width, height = img.size
+    draw = ImageDraw.Draw(img)
+    text = """Nithin K Joy
+    180928  """
+    font = ImageFont.truetype('arial.ttf', 36)
+    textwidth, textheight = draw.textsize(text, font)
+    margin = 10
+    x = width - textwidth - margin
+    y = height - textheight - margin
+    draw.text((x, y), text,fill=(255,0,0), font=font)
     img.save(f"image/{location}/{image}")
-print("done")    
+print("done")
